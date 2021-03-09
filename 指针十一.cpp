3277d304d97acf150a657f50eb6a3d6011166887
sforn-null <1641832926@qq.com>
2021-03-09 23:04:26 +0800

    指针练习十一

---
+++ 指针十一.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//参数是数组的形式
+//之前最常见的一种遍历二维数组的方式
+void print1(int arr[3][5], int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0;i < x;i++)
+	{
+		for (j = 0;j < y;j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+	print1(arr,3,5);
+	return 0;
+}
