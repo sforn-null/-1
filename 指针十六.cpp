e04e6d101a3cc54f236dd9420515ef4544b22475
sforn-null <1641832926@qq.com>
2021-03-12 22:50:37 +0800

    指针十六

---
+++ 指针十六.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+//函数指针数组
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}int Div(int x, int y)
+{
+	return x / y;
+}
+
+int main()
+{
+	//指针数组
+	int* arr[5];
+	//需要一个数组，这个数组可以存放4个函数的地址-函数指针的数组
+	int(*pa)(int, int) = Add;
+	int(*parr[4])(int, int) = { Add,Sub,Mul,Div };//函数指针的数组
+	int i = 0;
+	for (i = 0;i < 4;i++)
+	{
+		printf("%d\n", parr[i](2, 3));
+	}
+	return 0;
+}
